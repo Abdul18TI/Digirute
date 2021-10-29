8396495b6840b1e8eb405adfbd8372d1365dd1ee
andrahafiz <andra18ti@mahasiswa.pcr.ac.id>
2021-10-29 21:28:51 +0700

    Update database.php

--- application/config/database.php
+++ application/config/database.php
@@ -1,5 +1,5 @@
 <?php
-defined('BASEPATH') OR exit('No direct script access allowed');
+defined('BASEPATH') or exit('No direct script access allowed');
 
 /*
 | -------------------------------------------------------------------
@@ -75,8 +75,8 @@ $query_builder = TRUE;
 
 $db['default'] = array(
 	'dsn'	=> '',
-	'hostname' => 'localhost',
-	'username' => '',
+	'hostname' => 'db_digirute',
+	'username' => 'root',
 	'password' => '',
 	'database' => '',
 	'dbdriver' => 'mysqli',
