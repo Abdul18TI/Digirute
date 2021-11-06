3495256a49d18eab56564d32704312fe0dbb388a
abdulArif <52515011+Abdul18TI@users.noreply.github.com>
2021-11-06 22:10:55 +0700

    Model_tb_agenda

--- application/models/M_agenda.php
+++ application/models/M_agenda.php
@@ -6,7 +6,36 @@ class M_agenda extends CI_Model
     {
         $this->db->select('*');
         $this->db->from('tb_agenda');
+        $this->db->join('tb_foto_agenda', 'tb_agenda.ID_Agenda = tb_foto_agenda.ID_Agenda');
         $query = $this->db->get();
         return $query;
+    }
+
+    public function tampil_data_agenda_by_id($ID_Agenda)
+    {
+        $this->db->select('*');
+        $this->db->from('tb_agenda');
+        $this->db->join('tb_foto_agenda', 'tb_agenda.ID_Agenda = tb_foto_agenda.ID_Agenda');
+        $this->db->where('ID_Agenda', $ID_Agenda);
+        $query = $this->db->get();
+        return $query;
+    }
+
+    public function tampil_gambar_agenda_by_id_agenda($ID_Agenda)
+    {
+        $this->db->select('*');
+        $this->db->from('tb_foto_agenda');
+        $this->db->where('ID_Agenda', $ID_Agenda);
+        $query = $this->db->get();
+        return $query;
+    }
+
+    public function hapus_agenda($ID_Agenda)
+    {
+        $data['tb_agenda'] = $this->db->get_where('tb_agenda', ['ID_Agenda' => $ID_Agenda])->row_array();
+        $data['tb_foto_agenda'] = $this->db->get_where('tb_foto_agenda', ['ID_Agenda' => $ID_Agenda])->row_array();
+        $this->db->delete('tb_foto_agenda', ['ID_Agenda' => $ID_Agenda]);
+        unlink(FCPATH . 'assets/img/foto_product/' . $data['tb_foto_agenda']['Foto_Agenda']);
+        redirect('Product_a');
     }
 }
---
+++ application/views/RW/Form/form_agenda.php
@@ -0,0 +1,54 @@
+<div class="page-body">
+    <div class="container-fluid">
+        <div class="page-header">
+            <div class="row">
+                <div class="col-sm-6">
+                </div>
+            </div>
+        </div>
+    </div><!-- Form Pengaduan -->
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-sm-12">
+                <div class="card">
+                    <div class="card-header pb-0">
+                        <h5>Form Tambah Agenda</h5>
+                    </div>
+                    <form class="form theme-form">
+                        <div class="card-body">
+                            <div class="row">
+                                <div class="col">
+                                    <div class="mb-3">
+                                        <label class="form-label" for="exampleInputPassword22">Nama Agenda : </label>
+                                        <input class="form-control" id="exampleInputPassword22" type="text" placeholder="Gotong royong">
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="row">
+                                <div class="col">
+                                    <div>
+                                        <label class="form-label" for="exampleFormControlTextarea4">Isi Agenda : </label>
+                                        <textarea class="form-control" id="exampleFormControlTextarea4" rows="3"></textarea>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="row">
+                                <div class="col">
+                                    <div>
+                                        <label class="form-label" for="exampleFormControlTextarea4">Foto Agenda : </label>
+                                        <input class="form-control" type="file">
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="card-footer text-end">
+                            <button class="btn btn-primary" type="submit">Tambah</button>
+                            <input class="btn btn-light" type="reset" value="Batal">
+                        </div>
+                    </form>
+                </div>
+                <!-- Form Pengaduan End -->
+            </div>
+        </div>
+    </div>
+</div>
