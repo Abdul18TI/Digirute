1555c2cc7cccafbc943909a12f4cba33c38968f8
andrahafiz <52515217+andrahafiz@users.noreply.github.com>
2021-11-13 23:03:51 +0700

    Update sidebar_rt.php

--- application/views/Templates/sidebar_rt.php
+++ application/views/Templates/sidebar_rt.php
@@ -3,11 +3,11 @@
     <!-- Page Sidebar Start-->
     <header class="main-nav">
         <div class="sidebar-user text-center"><a class="setting-primary" href="javascript:void(0)"><i data-feather="settings"></i></a><img class="img-90 rounded-circle" src="<?= base_url() ?>assets/images/dashboard/1.png" alt="">
-            <div class="badge-bottom"><span class="badge badge-primary">New</span></div><a href="user-profile.html">
-                <h6 class="mt-3 f-14 f-w-600">Emay Walter</h6>
+            <div class="badge-bottom"><span class="badge badge-primary">RT</span></div><a href="user-profile.html">
+                <h6 class="mt-3 f-14 f-w-600">Sahid</h6>
             </a>
-            <p class="mb-0 font-roboto">Human Resources Department</p>
-            <ul>
+            <p class="mb-0 font-roboto">RT 04 RW 01</p>
+            <!-- <ul>
                 <li><span><span class="counter">19.8</span>k</span>
                     <p>Follow</p>
                 </li>
@@ -17,7 +17,7 @@
                 <li><span><span class="counter">95.2</span>k</span>
                     <p>Follower </p>
                 </li>
-            </ul>
+            </ul> -->
         </div>
         <nav>
             <div class="main-navbar">
